61140f54d1118a289a24e0cf9a365e564c7c1b86
paclosap <paclosap@lavanguardia.es>
2025-11-25 12:56:40 +0100

    Add files via upload

---
+++ pedreaPRUEBA.js
@@ -0,0 +1,3 @@
+/* Datos proporcionados por Europa Press. Queda prohibida su reproduccion total o parcial sin el consentimiento expreso y por escrito. Copyright de esta compilacion: Europa Press - 2024 */
+var pedrea = [];
+var pedreaActualizacion = "2024-12-22T16:05:00";
