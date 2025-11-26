37b4cc6086284e99fbdbcae981bd38167c3647f1
paclosap <paclosap@lavanguardia.es>
2025-11-26 13:04:21 +0100

    Update primerosPRUEBA.js

--- primerosPRUEBA.js
+++ primerosPRUEBA.js
@@ -1,4 +1,8 @@
 /* Datos proporcionados por Europa Press. Queda prohibida su reproduccion total o parcial sin el consentimiento expreso y por escrito. Copyright de esta compilacion: Europa Press - 2024 */
 var primeros = [72480,
-40014];
+40014,
+11840,
+48020,77768,
+37876,45225,45456,60622,72853,74778,75143,97345];
 var primerosActualizacion = "2024-12-22T16:05:00"
+
